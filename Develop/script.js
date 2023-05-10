5720ad578f42816b72aa69c41a3f2fe50ab999d3
Yasmin Castillo <yasmin_c@outlook.com>
2023-05-10 09:56:18 -0500

    added arrays for numbers,letters, and special characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,12 @@
 // Assignment code here
+lowercaseAlphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","o","p","q","r","s","t","u","v","w","x","y","z"];
+uppercaseAlphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+numbers = ["1","2","3","4","5","6","7","8","9","0"];
+specialCharacters = ["!","@","#","$","%","^","&","*"];
 
+function generatePassword(length,lowercase,uppercase,numbers,characters){
+  console.log("How long do you want your password to be? Choose between 8 and 128 characters")
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
